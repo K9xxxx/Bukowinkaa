51c5aed538e378acdb912da2161919430ef8a3b0
Patryk Jary <49764884+K9xxxx@users.noreply.github.com>
2025-01-03 04:40:40 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -4,6 +4,25 @@ const navIcon = document.querySelector('.navigation-icon');
 const closeIcon = document.querySelector('.close-icon');
 const mobNavigation = document.querySelector('.mobile-navigation');
 
+
+
+let scrollPosition = 0;
+
+// Zablokowanie przewijania
+function disableScroll() {
+  scrollPosition = window.pageYOffset;
+  document.body.style.position = 'fixed';
+  document.body.style.top = `-${scrollPosition}px`;
+  document.body.style.width = '100%';
+}
+
+// Odblokowanie przewijania
+function enableScroll() {
+  document.body.style.position = '';
+  document.body.style.top = '';
+  window.scrollTo(0, scrollPosition);
+}
+
 // Dodaj nasłuchiwanie na zdarzenie przewijania strony
 window.addEventListener('scroll', () => {
     if (window.scrollY > 150) {
@@ -16,8 +35,11 @@ window.addEventListener('scroll', () => {
 navIcon.addEventListener('click',()=>{
     navIcon.classList.add('sleeping');
     mobNavigation.classList.remove('sleeping')
+    disableScroll(); // Zablokuj
 })
 closeIcon.addEventListener('click',()=>{
     navIcon.classList.remove('sleeping');
     mobNavigation.classList.add('sleeping')
+    enableScroll();  // Odblokuj
+    
 })
